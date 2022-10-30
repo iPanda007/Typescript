6114fa37d9cfb130371b30a5ab8288be3dd8aa6d
iPanda <aungkyawkhaing2000@gmail.com>
2022-10-30 23:47:26 +0630

    gg

---
+++ chaper2/interface.js
@@ -0,0 +1,12 @@
+"use strict";
+let point = {
+    x: 10,
+    y: 20
+};
+let k;
+k = 3;
+let m;
+m = {
+    bb: "akk",
+    cc: 4
+};
---
+++ chaper2/interface.ts
@@ -0,0 +1,23 @@
+interface Point{
+    x:number;
+    y:number;
+}
+
+let point :Point = {
+    x:10,
+    y:20
+} 
+
+type OurNumber = Number;
+let k : OurNumber;
+k= 3;
+
+type Hle = {
+    bb : string;
+    cc:number
+}
+let m : Hle; 
+m ={
+    bb : "akk",
+    cc: 4
+} 
