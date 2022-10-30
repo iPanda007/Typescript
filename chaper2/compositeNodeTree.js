fb9d18ddd629c79f38005ae204aa1573176054ce
iPanda <aungkyawkhaing2000@gmail.com>
2022-10-30 23:25:52 +0630

    add.

---
+++ chaper2/compositeNodeTree.js
@@ -0,0 +1,11 @@
+"use strict";
+let tree = {
+    children: [
+        { data: "Aung Kyaw Khaing" },
+        { children: [
+                { children: [
+                        { data: "aa" }
+                    ] }
+            ] },
+    ]
+};
---
+++ chaper2/compositeNodeTree.ts
@@ -0,0 +1,19 @@
+type LeafNode= {
+    data:string
+}
+type TreeNode = {
+    children:Array<Tree>
+}
+
+type Tree = LeafNode | TreeNode;
+
+let tree : Tree = {
+    children: [
+        {data:"Aung Kyaw Khaing"},
+        {children:[
+           {children:[
+             {data:"aa"}
+           ]}
+        ]},
+    ]
+}
